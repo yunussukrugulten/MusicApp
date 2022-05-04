da1bd7d25a6dec7cf2fa3740efaeb79902b11f66
Yunus Sukru Gulten <yunussukrugulten@gmail.com>
2022-05-04 18:15:21 +0300

    first commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    TextInput,
+    View,
+} from 'react-native';
+import styles from './SearchBar.styles';
+
+const SearchBar = ({onSearch}) => {
+    return (
+        <View style={styles.container}>
+            <TextInput
+            placeholder="Ara..."
+            onChangeText={onSearch}
+            />
+        </View>
+    );
+};
+
+export default SearchBar;
